e5102191dd0771dffa82b43cfa01046f5a7fb7fd
applight <admin@app.lighting>
2021-11-28 02:12:58 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <div class="nav">
+    </div>
+    
+    <div class="content">
+      <div class="customers">
+      </div>
+      <div class="controls">
+      </div>
+    </div>
+    
+    <div class="footer">
+    </div>
+  
+  </body>
+</html>
