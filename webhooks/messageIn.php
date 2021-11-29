e19589aa6ed905c97df68d146c849ee4133d563b
Matt Vaughan <admin@thenshow.me>
2021-11-29 01:22:29 -0500

    Fix concatenation

--- webhooks/messageIn.php
+++ webhooks/messageIn.php
@@ -1,20 +1,24 @@
 <?php
-
 // Required if your environment does not handle autoloading
-require_once '../../vendor/autoload.php';
-
+// require_once '../../vendor/autoload.php';
 // Use the REST API Client to make requests to the Twilio REST API
-use Twilio\Rest\Client;
+// use Twilio\Rest\Client;
+
+echo "before anything but autoload";
 
 $sid   = getenv('TWILIO_ACCOUNT_SID');
 $token = getenv('TWILIO_AUTH_TOKEN');
-$client = new Client($sid, $token);
+echo "sid: " . $sid;
+
+$client = new Twilio\Rest\Client($sid, $token);
 
 $msg = $client->message->create(
 	$_POST['From'],
 	[ 'from' => $_POST['To'],
-	  'body' => 'My name is cuban pete and my friend said ' + $_POST['Body']
+	  'body' => 'My name is cuban pete and my friend said ' . $_POST['Body']
 	]);
 	
 echo $msg->sid;
+
+
 ?>
