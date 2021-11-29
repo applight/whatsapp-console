bc186f1427bbd79b4a69da6a246767853a02b819
Matt Vaughan <admin@thenshow.me>
2021-11-28 21:17:08 -0500

    Add a webhook for whatsapp messages coming in to Twilio

--- webhooks/messageIn.php
+++ webhooks/messageIn.php
@@ -1,4 +1,19 @@
 <?php
 
+// Required if your environment does not handle autoloading
+require_once __DIR__ . '/vendor/autoload.php';
 
+// Use the REST API Client to make requests to the Twilio REST API
+use Twilio\Rest\Client;
+
+$sid   = getenv('TWILIO_ACCOUNT_SID');
+$token = getenv('TWILIO_AUTH_TOKEN');
+$client = new Client($sid, $token);
+
+$msg = $client->message->create(
+	$_POST['From'],
+	[ 'from' => $_POST['To'],
+	  'body' => 'My name is cuban pete and my friend said ' + $_POST['Body']
+	]);
+echo $msg->sid;
 ?>
