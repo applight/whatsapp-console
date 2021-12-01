4c8020e5ae12620b755f22d7832cb044bea0fa94
Matt Vaughan <admin@thenshow.me>
2021-12-01 12:09:52 -0500

    more style

--- webroot/whatsapp.php
+++ webroot/whatsapp.php
@@ -9,6 +9,24 @@
 </div>
 <div class="chats">
 <?php
-          
+require_once __DIR__ . '/vendor/autoload.php';
+use Twilio\Rest\Client;
+
+$sid = getenv("TWILIO_ACCOUNT_SID");
+$token = getenv("TWILIO_AUTH_TOKEN");
+$twilio = new Client($sid, $token);
+
+$messages = $twilio->messages->read([], 50);
+
+foreach ($messages as $msq) {
+    echo "<div class=\"customer\" ><ul>";
+
+    $msg = $twilio->messages->read([ "from" => $from ], 20);
+    foreach($messages_from as $message_from) {
+        echo "<li>" . $message_from->body . "</li>";
+    }
+        
+    echo "</ul></div>";
+}
 ?>
 </div></body></html>
