bc4552a4ce9b12f1ff655c106891ecd2d04a1b3c
Matt Vaughan <admin@thenshow.me>
2021-12-01 10:32:50 -0500

    correct setup

--- webroot/list-recent.php
+++ webroot/list-recent.php
@@ -1,7 +1,7 @@
 <?php
 // Update the path below to your autoload.php,
 // see https://getcomposer.org/doc/01-basic-usage.md
-require_once __DIR__ . '/vendor/autoload.php';
+require_once '/vendor/autoload.php';
 use Twilio\Rest\Client;
 
 class Set {
